89cc0262f071c4f0f6b71b2f83002f91a261991b
samdev <samuantonio14@gmail.com>
2024-12-19 07:55:38 -0600

    Mejoras del sistema de yes-entregas

--- vite.config.mjs
+++ vite.config.mjs
@@ -25,15 +25,5 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-
-    server: {
-       proxy: {
-            '/api': {
-                target: 'http://localhost:3000', // Cambia esto a la URL de tu servidor Express
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    }
     
 });
