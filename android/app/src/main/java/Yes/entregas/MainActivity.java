2c6546eba89c8c24f03947b648033a215f6a8af7
samdev <samuantonio14@gmail.com>
2024-12-23 08:32:57 -0600

    Fixes

--- android/app/src/main/java/Yes/entregas/MainActivity.java
+++ android/app/src/main/java/Yes/entregas/MainActivity.java
@@ -1,5 +1,6 @@
 package Yes.entregas;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.database.sqlite.CapacitorSQLite;
 
 public class MainActivity extends BridgeActivity {}
