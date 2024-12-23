e6a57242af3ffa0d267cd9d2ff696734526a86b0
samdev <samuantonio14@gmail.com>
2024-12-22 22:14:53 -0600

    first commitssss

--- src/api-plugins/entregaService.js
+++ src/api-plugins/entregaService.js
@@ -1,34 +1,25 @@
 // api-plugins/entregaService.js
-import { SQLite } from '@capacitor-community/sqlite';
+import { CapacitorSQLite } from '@capacitor-community/sqlite'; // Usa este nombre correcto
 
 export const getLogs = async () => {
   try {
-    const db = await SQLite.create({
+    const db = await CapacitorSQLite.create({
       name: 'yesentregas',
       location: 'default',
     });
-
     await db.open();
 
-    // Consulta los logs en la tabla 'log'
-    const query = 'SELECT * FROM log'; 
+    const query = 'SELECT * FROM logs';
     const result = await db.executeSql(query);
 
-    // Si los logs están vacíos, retornar un arreglo vacío
-    if (result.rows.length === 0) {
-      return [];
-    }
-
-    const logs = [];
+    let logs = [];
     for (let i = 0; i < result.rows.length; i++) {
       logs.push(result.rows.item(i));
     }
-
-    db.close();
+    db.close(); // Asegúrate de cerrar la base de datos después de la consulta
     return logs;
   } catch (error) {
-    console.error('Error al obtener los logs: ', error);
-    return []; // En caso de error, retornar un arreglo vacío
-    
+    console.error('Error al cargar los logs:', error);
+    throw error;
   }
 };
